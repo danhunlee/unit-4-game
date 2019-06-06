7f7f38c2631bdc68c853249b08ec8a6d135605c4
Hun Lee <dan.hun.lee@gmail.com>
2019-06-06 00:14:12 -0700

    hl

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -24,84 +24,116 @@ var losses = 0;
 // Calling a function so computer generates a random number.
 // I wanna have the computer to guess a random number and shows it on screen before the user starts 
 // Computer is generating random number 19-120.
+// TODO: FIX THE GUESS NUMBER TO BE ZERO WHEN YOU RESET THE GAME. 
 
-$(document).ready(function() {
-    
+$(document).ready(function () {
+
     randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     $("#computer-score").text(randomCompNum);
-    console.log(randomCompNum);   
+    console.log(randomCompNum);
 
     // I wanna have 4 different crystals generating random number 1-12.
     crystalNum1 = Math.floor(Math.random() * (12)) + 1;
     console.log(crystalNum1);
     crystalNum2 = Math.floor(Math.random() * (12)) + 1;
     console.log(crystalNum2);
-    var crystalNum3 = Math.floor(Math.random() * (12)) + 1;
+    crystalNum3 = Math.floor(Math.random() * (12)) + 1;
     console.log(crystalNum3);
-    var crystalNum4 = Math.floor(Math.random() * (12)) + 1;
+    crystalNum4 = Math.floor(Math.random() * (12)) + 1;
     console.log(crystalNum4);
 
-    $(".crystal1").on("click", function() {
+    $(".crystal1").on("click", function () {
         guessTotal += crystalNum1;
         $("#userGuess-Total").text(guessTotal);
-        if(guessTotal === randomCompNum) {
+        if (guessTotal === randomCompNum) {
             alert("You win!")
             wins++;
             $("#wins").text(wins);
-        }
-        else if(guessTotal > randomCompNum) {
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum1 = Math.floor(Math.random() * (12)) + 1;
+
+        } else if (guessTotal > randomCompNum) {
             alert("You lost!")
             losses++;
             $("#losses").text(losses);
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum1 = Math.floor(Math.random() * (12)) + 1;
+
         }
     });
 
-    $(".crystal2").on("click", function() {
+    $(".crystal2").on("click", function () {
         guessTotal += crystalNum2;
         $("#userGuess-Total").text(guessTotal);
-        if(guessTotal === randomCompNum) {
+        if (guessTotal === randomCompNum) {
             alert("You win!")
             wins++;
             $("#wins").text(wins);
-        }
-        else if(guessTotal > randomCompNum) {
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum2 = Math.floor(Math.random() * (12)) + 1;
+
+        } else if (guessTotal > randomCompNum) {
             alert("You lost!")
             losses++;
             $("#losses").text(losses);
-        }
-    }); 
-    
-    $(".crystal3").on("click", function() {
-        guessTotal += crystalNum3;
-        $("#userGuess-Total").text(guessTotal);
-        if(guessTotal === randomCompNum) {
-            alert("You win!")
-            wins++;
-            $("#wins").text(wins);
-        }
-        else if(guessTotal > randomCompNum) {
-            alert("You lost!")
-            losses++;
-            $("#losses").text(losses);
-        }
-    }); 
-    
-    $(".crystal4").on("click", function() {
-        guessTotal += crystalNum4;
-        $("#userGuess-Total").text(guessTotal);
-        if(guessTotal === randomCompNum) {
-            alert("You win!")
-            wins++;
-            $("#wins").text(wins);
-        }
-        else if(guessTotal > randomCompNum) {
-            alert("You lost!")
-            losses++;
-            $("#losses").text(losses);
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum2 = Math.floor(Math.random() * (12)) + 1;
+
         }
     });
 
-  
-    
+    $(".crystal3").on("click", function () {
+        guessTotal += crystalNum3;
+        $("#userGuess-Total").text(guessTotal);
+        if (guessTotal === randomCompNum) {
+            alert("You win!")
+            wins++;
+            $("#wins").text(wins);
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum3 = Math.floor(Math.random() * (12)) + 1;
+
+        } else if (guessTotal > randomCompNum) {
+            alert("You lost!")
+            losses++;
+            $("#losses").text(losses);
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum3 = Math.floor(Math.random() * (12)) + 1;
+
+        }
+    });
+
+    $(".crystal4").on("click", function () {
+        guessTotal += crystalNum4;
+        $("#userGuess-Total").text(guessTotal);
+        if (guessTotal === randomCompNum) {
+            alert("You win!")
+            wins++;
+            $("#wins").text(wins);
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum4 = Math.floor(Math.random() * (12)) + 1;
+        } else if (guessTotal > randomCompNum) {
+            alert("You lost!")
+            losses++;
+            $("#losses").text(losses);
+            guessTotal = 0;
+            randomCompNum = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+            $("#computer-score").text(randomCompNum);
+            crystalNum4 = Math.floor(Math.random() * (12)) + 1;
+        }
+    });
 
 });
